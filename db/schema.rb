2fcdf7dc57f3125eb2548de2833d11a9d2ea6807
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2023-11-28 03:29:04 +0000

    generate like and comment

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_032816) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_28_032846) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "photo_id"
+    t.integer "author_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "follow_requests", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "recipient_id"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "photo_id"
+    t.integer "fan_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
