3450f537c3848b0657eb7edab98684f397e6d362
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2023-11-30 01:12:53 +0000

    semi broke image uploader

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,6 +12,9 @@
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
+
+  mount_uploader :image, ImageUploader
+
   belongs_to :user, required: true, class_name: "User", foreign_key: "owner_id"
   has_many  :comments, class_name: "Comment", foreign_key: "photo_id", dependent: :destroy
   has_many  :likes, class_name: "Like", foreign_key: "photo_id", dependent: :destroy
