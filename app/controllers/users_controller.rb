272c7f1b7ebbf7c95a507a8ed57fc52edb4afee7
donnellycarroll <146589442+donnellycarroll@users.noreply.github.com>
2023-12-05 23:23:20 +0000

    started on users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,13 @@ class UsersController < ApplicationController
 
     render({ :template => "users/index" })
   end
+
+  def show
+    the_name = params.fetch("path_id")
+
+    @the_user = User.where({ :username => the_name })
+
+    render({ :template => "users/show" })
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -88,4 +88,6 @@ Rails.application.routes.draw do
   
   get("/users", { :controller => "users", :action => "index"  })
 
+  get("/users/:path_id", { :controller => "users", :action => "show" })
+
 end
